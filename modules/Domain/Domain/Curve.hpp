7a00593451b67c8694d4d64e9279717c02d2d7fd
ltjax <marius.elvert@googlemail.com>
2017-05-12 00:39:48 +0200

    Added curve type editing

--- modules/Domain/Domain/Curve.hpp
+++ modules/Domain/Domain/Curve.hpp
@@ -13,6 +13,7 @@ public:
 	Curve(FunctionType Type=FunctionType::Linear, float m=1.f, float k=1.f, float c=0.f, float b=0.f);
 	float evaluateFor(float x) const;
 
+	Curve withType(FunctionType type) const { return { type, mM, mK, mC, mB }; }
 	Curve withM(float m) const { return { mType, m, mK, mC, mB }; }
 	Curve withK(float k) const { return { mType, mM, k, mC, mB }; }
 	Curve withC(float c) const { return { mType, mM, mK, c, mB }; }
--- modules/Frontend/Frontend/AxisWidget.cpp
+++ modules/Frontend/Frontend/AxisWidget.cpp
@@ -1,5 +1,17 @@
 #include "AxisWidget.hpp"
 #include "ui_Axis.h"
+#include <unordered_map>
+
+namespace {
+
+
+std::vector<std::pair<Curve::FunctionType, QString>> const typeToString = {
+	{ Curve::FunctionType::Linear, "Linear" },
+	{ Curve::FunctionType::Polynomial, "Polynomial" },
+	{ Curve::FunctionType::Logistic, "Logistic" }
+};
+
+}
 
 AxisWidget::AxisWidget(QWidget * parent)
 : mUi(std::make_unique<Ui::Axis>())
@@ -22,6 +34,17 @@ AxisWidget::AxisWidget(QWidget * parent)
 	connect(mUi->b, valueChanged, [this](double value)
 	{
 		mUi->graphWidget->setCurve(mUi->graphWidget->getCurve().withB(value));
+	});
+
+	for (int i = 0; i < typeToString.size(); ++i)
+		mUi->type->addItem(typeToString[i].second, QVariant(static_cast<int>(typeToString[i].first)));
+
+	auto currentIndexChanged = static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged);
+	connect(mUi->type, currentIndexChanged, [this](int current)
+	{
+		if (current < typeToString.size())
+			mUi->graphWidget->setCurve(mUi->graphWidget->getCurve().withType(typeToString[current].first));
+			
 	});
 }
 
