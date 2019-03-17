0d81d51e0eaf45e9c4c852460755610439edfc69
ltjax <marius.elvert@googlemail.com>
2019-03-17 21:30:19 +0100

    Use table model for output

--- modules/Rescue/OutputListWidget.cpp
+++ modules/Rescue/OutputListWidget.cpp
@@ -3,13 +3,15 @@
 using namespace Rescue;
 
 OutputListWidget::OutputListWidget(ushiro::state_observer<Rescue::State> observer, QWidget* parent)
-: QWidget(parent)
+: QTableView(parent)
 , mObserver(std::move(observer))
 {
-  mLayout = new QFormLayout(this);
+  mLayout = new QVBoxLayout(this);
   setLayout(mLayout);
 
-  mObserver.observe([](State const& state) { return std::tie(state.outputs, state.group); },
+  this->setModel(&mModel);
+
+  mObserver.observe([](Rescue::State const& state) { return std::tie(state.outputs, state.group); },
                     [this](Outputs const& outputs, Group const& group) { return updateFrom(outputs, group); });
 }
 
@@ -17,15 +19,68 @@ OutputListWidget::~OutputListWidget() = default;
 
 void OutputListWidget::updateFrom(Outputs const& outputs, Group const& group)
 {
-  // Clear all
-  while (mLayout->rowCount())
-  {
-    mLayout->removeRow(mLayout->rowCount() - 1);
-  }
+  mModel.updateFrom(outputs, group);
+}
+
+int OutputTreeModel::rowCount(QModelIndex const& parent) const
+{
+  return mNames.size();
+}
+
+int OutputTreeModel::columnCount(QModelIndex const& parent) const
+{
+  return 2;
+}
+
+QVariant OutputTreeModel::data(const QModelIndex& index, int role) const
+{
+  int row = index.row();
+  int col = index.column();
+
+  if (role != Qt::DisplayRole || row > mNames.size())
+    return QVariant();
+
+  if (col == 0)
+    return mNames[row];
+  if (col == 1)
+    return mValues[row];
+
+  return QVariant();
+}
+
+void OutputTreeModel::updateFrom(Outputs const& outputs, Group const& group)
+{
+  beginResetModel();
+
+  mNames.clear();
+  mValues.clear();
 
   for (auto const& output : outputs)
   {
     auto name = locate(group, output->id)->name;
-    mLayout->addRow(new QLabel(name.c_str(), this), new QLabel(QString("%1").arg(output->value), this));
+    mNames.push_back(name.c_str());
+    mValues.push_back(output->value);
   }
+
+  endResetModel();
+  if (mNames.empty())
+    return;
+
+//  QModelIndex topLeft = createIndex(0,0);
+//  QModelIndex bottomRight = createIndex(mNames.size()-1, 1);
+//  emit dataChanged(topLeft, bottomRight, {Qt::DisplayRole});
+  //emit modelReset();
+}
+
+QVariant OutputTreeModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+  if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
+    switch (section) {
+      case 0:
+        return QString("Action");
+      case 1:
+        return QString("Weight");
+    }
+  }
+  return QVariant();
 }
--- modules/Rescue/OutputListWidget.hpp
+++ modules/Rescue/OutputListWidget.hpp
@@ -4,22 +4,41 @@
 #include <QtWidgets/QFormLayout>
 #include <diffable_list.hpp>
 #include <QtWidgets/QLabel>
+#include <QtCore/QAbstractTableModel>
+#include <QtWidgets/QTableView>
 #include "state_observer.hpp"
 #include "State.hpp"
 
 namespace Rescue
 {
 
-class OutputListWidget : public QWidget
+class OutputTreeModel : public QAbstractTableModel
 {
 public:
-  OutputListWidget(ushiro::state_observer<State> observer, QWidget* parent);
+  void updateFrom(Outputs const& outputs, Group const& group);
+
+  int rowCount(QModelIndex const& parent) const override;
+  int columnCount(QModelIndex const& parent) const override;
+
+  QVariant data(const QModelIndex& index, int role) const override;
+
+  QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+private:
+  QStringList mNames;
+  std::vector<float> mValues;
+};
+
+class OutputListWidget : public QTableView
+{
+public:
+  OutputListWidget(ushiro::state_observer<Rescue::State> observer, QWidget* parent);
   ~OutputListWidget();
 
 private:
   void updateFrom(Outputs const& outputs, Group const& group);
   ushiro::state_observer<Rescue::State> mObserver;
-  QFormLayout* mLayout;
+  QVBoxLayout* mLayout;
+  OutputTreeModel mModel;
 };
 
 }
